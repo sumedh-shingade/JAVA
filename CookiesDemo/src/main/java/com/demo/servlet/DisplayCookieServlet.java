02197d85686e082ee643429371dc7d03e2d86564
sumedh-shingade <44223852+sumedh-shingade@users.noreply.github.com>
2023-06-24 16:40:53 +0500

    Add files via upload

---
+++ CookiesDemo/src/main/java/com/demo/servlet/DisplayCookieServlet.java
@@ -0,0 +1,44 @@
+package com.demo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet( "/display" )
+public class DisplayCookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	
+	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+	//retrive all cookie from request object and display it 
+		Cookie[] cookies=request.getCookies();
+		
+		for(Cookie c:cookies) {
+			out.println("Name : "+c.getName()+ " value : "+c.getValue()+"<br>");
+		}
+		
+		RequestDispatcher rd=request.getRequestDispatcher("cookiedata.html");
+		rd.include(request,response);
+		
+	}
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+
+
+}
